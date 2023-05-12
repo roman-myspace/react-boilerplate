f9267661f712eb08ccd8348c70f6edf0f8e3e78a
roman-myspace <103169678+roman-myspace@users.noreply.github.com>
2023-05-12 17:32:55 +0500

    svg icon loader component

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,4 +12,3 @@ export function formatBytes(bytes, decimals = 2) {
     const i = Math.floor(Math.log(bytes) / Math.log(k));
 
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + '' + sizes[i];
-}
